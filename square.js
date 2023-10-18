8a554f8ccbad481366f3ed61e23b7574a464dffa
Augustine Fuertes <141537663+MeadowOwner@users.noreply.github.com>
2023-10-17 22:56:22 -0700

    Set the default to 16 squares. Added functionality to new square button. Changed colors of borders and background.

--- square.js
+++ square.js
@@ -1,4 +1,4 @@
-const draw = document.querySelector('#drawing');
+squareSec = document.querySelector('#squareSec');
 
 function changeCol(box){
     box.style.backgroundColor = 'black';
@@ -7,20 +7,18 @@ function changeCol(box){
 
 function makeSquares(sideAmount){
     for (let i = 0; i < sideAmount; ++i){
+        const draw = document.querySelector('#drawing');
         const row = document.createElement('div');
-        row.style.borderStyle = 'solid';
-        row.style.borderColor = 'black';
-        row.style.borderWidth = '2px';
         row.style.flex = '1';
         row.style.display = "flex";
         row.style.flexDirection = 'row';
-        drawing.appendChild(row);
+        draw.appendChild(row);
     
         for(let j = 0; j < sideAmount; ++j){
             const square = document.createElement('div');
             square.style.borderStyle = 'solid';
-            square.style.borderColor = 'black';
-            square.style.borderWidth = '2px';
+            square.style.borderColor = 'lightGrey';
+            square.style.borderWidth = '1px';
             square.style.flex = '1';
             square.addEventListener('mouseover', () => {
                 changeCol(square);
@@ -31,4 +29,32 @@ function makeSquares(sideAmount){
     }
 }
 
-makeSquares(25);
+makeSquares(16);
+
+reset = function(){
+    let validIn = true;
+    let userNum = '';
+    do{
+        userNum = prompt('Enter the number of squares per side, between 0 and 100')
+        userNum = Number(userNum);
+        if((userNum == NaN) || (userNum < 0) || (userNum > 100)){
+            validIn = false;
+        }
+        else{
+            validIn = true;
+        }
+    }
+    while(!validIn);
+
+    let currDraw = document.querySelector('#drawing');
+
+    squareSec.removeChild(currDraw);
+    currDraw = document.createElement('div');
+    currDraw.id = 'drawing';
+    squareSec.appendChild(currDraw);
+
+    makeSquares(userNum);
+}
+
+resButt = document.querySelector('button');
+resButt.addEventListener('click', reset);
